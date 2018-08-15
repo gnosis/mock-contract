07b4137b590244b3389884b862504a146450fa1b
Felix Leupold <felixleupold90@gmail.com>
2018-08-15 17:52:17 +0200

    fix indentation

--- test/MockContract.js
+++ test/MockContract.js
@@ -6,23 +6,23 @@ const abi = require('ethereumjs-abi')
 
 contract('MockContract', function(accounts) {
   describe("givenReturn", function() {
-	  it("should return the mocked value", async function() {
-	  	const mock = await MockContract.new();
-		const complex = ComplexInterface.at(mock.address)
+    it("should return the mocked value", async function() {
+      const mock = await MockContract.new();
+      const complex = ComplexInterface.at(mock.address)
 
-		const encoded = await complex.contract.acceptAdressUintReturnBool.getData("0x0", 10);
-		await mock.givenReturn(encoded, abi.rawEncode(['bool'], [true]).toString());
-	  	
-	  	result = await complex.acceptAdressUintReturnBool.call("0x0", 10);
-	  	assert.equal(result, true)
-	  });
+      const encoded = await complex.contract.acceptAdressUintReturnBool.getData("0x0", 10);
+      await mock.givenReturn(encoded, abi.rawEncode(['bool'], [true]).toString());
+        
+      result = await complex.acceptAdressUintReturnBool.call("0x0", 10);
+      assert.equal(result, true)
+    });
 
-	  it("should return null if not mocked", async function() {
-	  	const mock = await MockContract.new();
-	  	const complex = ComplexInterface.at(mock.address)
+    it("should return null if not mocked", async function() {
+      const mock = await MockContract.new();
+      const complex = ComplexInterface.at(mock.address)
 
-	  	result = await complex.acceptAdressUintReturnBool.call("0x0", 10);
-	  	assert.equal(result, false)
-	  });
-	});
+      result = await complex.acceptAdressUintReturnBool.call("0x0", 10);
+      assert.equal(result, false)
+    });
+  });
 });
