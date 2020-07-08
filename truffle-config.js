1a914eec084296b6724e9dbc45092787da379532
Lecky Lao <leckylao@gmail.com>
2020-07-08 23:46:39 +1000

    adding networks into truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,6 +13,13 @@
  */
 
 module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    }
+  },
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   // Configure your compilers
@@ -22,8 +29,8 @@ module.exports = {
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
-         enabled: true,
-        //  runs: 200
+      enabled: true,
+      //  runs: 200
       //  },
       //  evmVersion: "istanbul"
       // }
