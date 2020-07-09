bb6595af09934c769443e43938bf4704aae614b5
Lecky Lao <leckylao@gmail.com>
2020-07-09 20:43:51 +1000

    fix and clean up truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,3 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
-
 module.exports = {
   networks: {
     development: {
@@ -20,20 +6,14 @@ module.exports = {
       network_id: "*"
     }
   },
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "v0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      enabled: true,
-      //  runs: 200
-      //  },
-      //  evmVersion: "istanbul"
-      // }
+      version: "v0.6.2",
+      settings: {
+        optimizer: {
+          enabled: true
+        },
+      }
     }
   }
 };
